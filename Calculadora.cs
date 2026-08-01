5253339e1b0988389b1f2eb6494b1670744abf24
Sofia <bisofsofia@gmail.com>
2026-08-01 16:37:25 -0300

    Projeo Calculadora em C#

---
+++ Calculadora.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aula_14
+{
+     public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} : {y} = {(double)x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double resultado = Math.Pow(x, y);
+
+            Console.WriteLine($"{x}^{y} = {resultado}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using aula_14;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(20,10);
+calc.Subtrair(10, 40);
+calc.Multiplicar(10, 20);
+calc.Dividir(8, 2);
+calc.Potencia(6, 6);
