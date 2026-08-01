019ace9dc95cd02ef3fd7240547acbf402d31791
Sofia <bisofsofia@gmail.com>
2026-08-01 16:48:37 -0300

    Adiciona gitignore

--- obj/Debug/net10.0/aula 14.AssemblyInfo.cs
+++ obj/Debug/net10.0/aula 14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula 14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380f3744c9c9816ee72a2064b3e936868269b240")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula 14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula 14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
